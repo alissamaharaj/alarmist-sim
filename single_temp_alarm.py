bd714d4709841a133eb3cc06f5f68077c5556d4f
alissamaharaj <alissamaharaj315@gmail.com>
2026-09-17 17:48:02 -0400

    Uploaded single temperature alarm code.

---
+++ single_temp_alarm.py
@@ -0,0 +1,12 @@
+temp = input("Temperature value:")
+try:
+    temp = float(temp)
+except:
+    print("Invalid temperature input")
+    quit()
+if 0 <= temp <= 55 :
+    print("-Normal-")
+elif -20 <= temp <= 0 or 55 <= temp <= 70 :
+    print("~Warning~")
+else:
+    print("!!CRITICAL!!")
